268b1797b556bcd6875cee4e4a380793eadd1c42
Eric Rini <eric.rini@gmail.com>
2019-04-09 14:03:26 -0400

    Alternative approach.

--- ContextLogger.js
+++ ContextLogger.js
@@ -1,72 +1,48 @@
-const asyncHooks = require('async_hooks');
-
-// Initialize a context for the main call stack (which is always asyncId = 1).
-const allContexts = {
-    1: {}
-};
-
-// Set the context for the main call stack active, since the application will always start in the main call stack.
-let activeContext = allContexts[1];
-
-// Create an async hook.
-const hook = asyncHooks.createHook({
-
-    // When a new call stack is created, initialize its context to a clone of the context from its parent's call stack.
-    init: (asyncId, type, triggerAsyncId, resource) => {
-
-        // I'm not super clear what the difference is between the executionId and triggerId. They are both some correlation
-        // to the parent call and thus to the call stack that the call belongs to. The executionId seems to correlate
-        // the user code function that is awaiting the event loop, while the triggerId seems to relate to anything that initiated 
-        // the action. Possibly outside of the user code. For example, when using promise/async magic the triggerId seems to 
-        // reference unknown calls. I'm guessing this has to do with the way async functions are internally broken up by the 
-        // runtime. In any case, using the executionId produces better results for this use case.
-        let contextId = asyncHooks.executionAsyncId();
-        
-        // This is tricksy, but we need to break the references between objects.
-        const clone = JSON.parse(JSON.stringify(allContexts[contextId]));
-
-        // This is purely for demo/debug purposes.
-        // clone.asyncId = asyncId;
-        // clone.triggerAsyncId = triggerAsyncId;
-        // clone.executionAsyncId = asyncHooks.executionAsyncId();
-
-        allContexts[asyncId] = clone;
-    },
-
-    // Each time a call stack joins the user thread, set its context active.
-    before: (asyncId) => {
-        activeContext = allContexts[asyncId];
-    },
-
-    // Each time a call stack completes, free the memory its context was using.
-    destroy: (asyncId) => {
-        delete allContexts[asyncId];
-    }
-});
-
-hook.enable();
-
-// This is pretty self explanatory?
-class ContextLogger
-{
-    set (key, value) {
-        activeContext[key] = value;
-    }
-
-    log () {
-        const args = Array.prototype.slice.call(arguments);
-        args.push(`(logger context = ${JSON.stringify(activeContext)})`);
-        console.log.apply(console, args);
-    }
-
-    toggleScope (value) {
-        if (value) {
-            hook.enable();
-        }
-        else {
-            hook.disable();
+class RootLogger {
+    beginScope(callback) {
+        if (callback) {
+            callback(new ChildLogger({}));
         }
     }
 }
 
-module.exports = ContextLogger;
+class ChildLogger {
+    constructor (scope) {
+        this._scope = scope;
+    }
+
+    setProperty (name, value) {
+        if (name) {
+            this._scope[name] = value;
+        }
+    }
+
+    logInfo () {
+        let serialized = "";
+
+        for (let name in this._scope) {
+            if (this._scope.hasOwnProperty(name)) {
+                if (serialized.length > 0) {
+                    serialized += "; ";
+                }
+
+                serialized += name;
+                serialized += " = ";
+                serialized += this._scope[name];
+            }
+        }
+
+        const args = Array.prototype.slice.call(arguments);
+        args.push(`(${serialized})`);
+        console.log.apply(console, args);
+    }
+
+    async beginScope(callback) {
+        if (callback) {
+            const clone = JSON.parse(JSON.stringify(this._scope));
+            await callback(new ChildLogger(clone));
+        }
+    }
+}
+
+module.exports = RootLogger;
--- app.js
+++ app.js
@@ -1,57 +1,39 @@
 const ContextLogger = require('./ContextLogger');
-
-// Note that this is a singleton. You could set it up such that this instance could be 
-// required directly or obtained via a global getInstance() method.
 const logger = new ContextLogger();
-logger.set("requestId", "???");
-logger.log("root log");
 
-// Create call stack "A". Think of this as an incoming request.
-setTimeout(function () {
-  logger.set("requestId", "A");
-  logger.log("begn");
-
-  setTimeout(function () {
-    logger.log("end");
-  }, 0);
-}, 0);
-
-// Create call stack "B". Think of this as an incoming request.
-setTimeout(function () {
-  logger.set("requestId", "B");
-  logger.log("begin");
-
-  // Let's have some fun...
-  setTimeout(function () {
-    logger.set("transactionId", "tx-1");
-    logger.log("database call 1");
-
-    setTimeout(function () {
-      logger.log("database call 2");
-
-      setTimeout(function () {
-        logger.log("end");
-      }, 0);
-    }, 0);
-  }, 0);
-}, 0);
-
-// Create call stack "C" using an async/await calling convention.
-(async function fn() {
-  await Promise.resolve().then(async function () {
-    logger.set("requestId", "C");
-    logger.set("key", "value-1");
-    logger.log("begin");
-
-    await Promise.resolve().then(async function () {
-      logger.set("key", "value-2");
-      logger.log("middle 1");
-
+(async function () {
+  await logger.beginScope(function (logger) {
+    logger.setProperty("requestId", "none");
+    logger.logInfo("server up");
+  
+    logger.beginScope(async function (logger) {
+      logger.setProperty("requestId", "A");
+      logger.setProperty("transactionId", 1);
+      logger.logInfo("Request to GET /something recieved.");
+  
       await Promise.resolve().then(function () {
-        logger.log("middle 2");
+        logger.setProperty("transactionId", 2);
+        logger.logInfo("SELECT * FROM table;");
       });
+  
+      logger.logInfo("HTTP 200 response sent.");
+    });
+  
+    logger.beginScope(async function (logger) {
+      logger.setProperty("requestId", "B");
+      logger.setProperty("transactionId", 1);
+      logger.logInfo("Request to GET /something recieved.");
+  
+      await logger.beginScope(async function (logger) {
+        await Promise.resolve().then(function () {
+          logger.setProperty("transactionId", 2);
+          logger.logInfo("SELECT * FROM table;");
+        });
+      })
+  
+      logger.logInfo("HTTP 200 response sent.");
     });
 
-    logger.log("end");
+    logger.logInfo("server down");
   });
 })();
