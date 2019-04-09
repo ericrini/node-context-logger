bc2952bf902403812bf44955e37d5267e482a2fe
Eric Rini <eric.rini@gmail.com>
2019-04-09 09:42:06 -0400

    Improve comment about triggerID vs executionId.

--- ContextLogger.js
+++ ContextLogger.js
@@ -14,8 +14,16 @@ const hook = asyncHooks.createHook({
     // When a new call stack is created, initialize its context to a clone of the context from its parent's call stack.
     init: (asyncId, type, triggerAsyncId, resource) => {
 
+        // I'm not super clear what the difference is between the executionId and triggerId. They are both some correlation
+        // to the parent call and thus to the call stack that the call belongs to. The executionId seems to correlate
+        // the user code function that is awaiting the event loop, while the triggerId seems to relate to anything that initiated 
+        // the action. Possibly outside of the user code. For example, when using promise/async magic the triggerId seems to 
+        // reference unknown calls. I'm guessing this has to do with the way async functions are internally broken up by the 
+        // runtime. In any case, using the executionId produces better results for this use case.
+        let contextId = asyncHooks.executionAsyncId();
+        
         // This is tricksy, but we need to break the references between objects.
-        const clone = JSON.parse(JSON.stringify(allContexts[asyncHooks.executionAsyncId()]));
+        const clone = JSON.parse(JSON.stringify(allContexts[contextId]));
 
         // This is purely for demo/debug purposes.
         // clone.asyncId = asyncId;
